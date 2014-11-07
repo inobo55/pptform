5b0753b0e456a63d92d370937932a2d1672635b3
inobo55 <inobo52@gmail.com>
2014-11-07 14:39:47 +0900

    一旦フォームできた

--- app/controllers/ppt_controller.rb
+++ app/controllers/ppt_controller.rb
@@ -2,12 +2,12 @@ class PptController < ApplicationController
 
   	def form
   		#フォーム表示
-  		
+  		@ppt = Ppt.new
   	end
 
   	def post
   		#DBに投稿する 
-
+      params.require(:ppt).permit(:name,:file)
   	end
 
   	def fetch
